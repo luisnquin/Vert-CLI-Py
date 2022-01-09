591b99476cfe28ce02827452e616f2f7ca582d37
luisnquin <lpaandres2020@gmail.com>
2022-01-09 18:42:38 -0500

    | Mod: src/main.py(newgoproject & newpyproject commands), src/utils.py(getconfigJSON -> target => ./src/config.json)  | Add: src/config.json, src/gen/gogen.py(generateGoProject), src/gen/pygen.py(generatePyProject)

--- src/gen/utils.py
+++ src/gen/utils.py
@@ -1,5 +1,19 @@
+import json
+
 
 def alternativeOpen(path: str, to_write: str):
     file = open(path, 'w', encoding='UTF-8')
     file.write(to_write)
     file.close()
+
+
+def getconfigJSON(configpath:str):
+    file = open(configpath, 'r')
+    content = json.load(file)
+    file.close()
+
+    return content
+
+
+if __name__ == '__main__':
+    pass
--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import json
-
 from rich.console import Console
 from rich.table import Table
 from rich.box import SQUARE
@@ -7,6 +5,8 @@ import typer
 
 from db.conn import DbConnection
 from db.models import *
+from gen.gogen import GenerateGoProject
+from gen.pygen import GeneratePyProject
 
 
 app = typer.Typer()
@@ -175,11 +175,29 @@ def deleteCategory(id: int = typer.Argument(None)):
 def deleteIdea(id: int = typer.Argument(None)):
     conn = DbConnection()
     if id:
-        query = TaskTable(id=id).delete()  # Wrong table
+        query = IdeaTable(id).delete()
         typer.echo(conn.execute(query=query))
         return
 
     typer.echo('Nothing to do, well')
+
+
+@app.command()
+def newgoproject(project_name=typer.Option(
+        None, prompt='Give me the name of your new Go project')):
+    """
+    If you want to configurate the path to your workspace, type
+    """
+    GenerateGoProject(project_name)
+
+
+@app.command()
+def newpyproject(project_name=typer.Option(
+        None, prompt='Give me the name of your new Py project')):
+    """
+    If you want to configurate the path to your workspace, type
+    """
+    GeneratePyProject(project_name)
 
 
 if __name__ == "__main__":
